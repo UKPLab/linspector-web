f2b449590b390642538b2473bd344d91ec98c2bb
Max Eichler <maexe@users.noreply.github.com>
2019-05-19 11:26:48 +0200

    Update to celery django backend

--- linspector/celery.py
+++ linspector/celery.py
@@ -4,6 +4,6 @@ import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'linspector.settings')
 
-app = Celery('linspector', broker='amqp://', backend='rpc://')
+app = Celery('linspector', broker='amqp://', backend='django-db')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
