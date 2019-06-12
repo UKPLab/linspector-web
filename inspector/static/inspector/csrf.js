7f977f57d1b329f1012d72ce72d3bbd38b192a20
Max Eichler <maexe@users.noreply.github.com>
2019-06-12 19:01:58 +0200

    Move upload JS to separate file

--- inspector/static/inspector/csrf.js
+++ inspector/static/inspector/csrf.js
@@ -1,9 +1,9 @@
 function getCookie(name) {
-    var cookieValue = null;
+    let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i].trim();
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
@@ -12,4 +12,4 @@ function getCookie(name) {
     }
     return cookieValue;
 }
-var csrftoken = getCookie('csrftoken');
+let csrftoken = getCookie('csrftoken');
